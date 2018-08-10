530eb621b2ca3be3944c725a404cb2e26df8cb73
PaulaAraya <paula.arayawilliams@gmail.com>
2018-08-09 23:12:27 -0300

    promesa

--- src/js/data.js
+++ src/js/data.js
@@ -1,12 +1,11 @@
 //Captura datos de API
 let bipCards = null;
 
-fetch("http://www.psep.cl/api/Bip.php?&numberBip=666")
+fetch('http://bip-servicio.herokuapp.com/api/v1/solicitudes.json?bip={id}')
 .then(response => response.json())
 .then(bipJSON => {
   bipCards = bipJSON
   console.log(bipCards);
-  renderInfoBip(bipCards);
 })
 .catch(error => {
   console.error("tarjeta no existe");
